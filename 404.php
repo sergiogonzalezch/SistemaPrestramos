122ba51eed60524129bfec0defb83db746f265ca
sergiogonzalezch <62730746+sergiogonzalezch@users.noreply.github.com>
2022-03-28 01:07:10 -0600

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,55 @@
+<div id="content">
+	<link rel="stylesheet" href="public/css/bootstrap.min.css">
+	<style>
+		<style>.box {
+			-webkit-box-pack: center;
+			-ms-flex-pack: center;
+			margin: 50px;
+			padding: auto;
+			justify-content: center;
+		}
+
+		.box div img {
+			width: 100%;
+		}
+
+		div {
+			font-family: Open sans;
+		}
+
+		a {
+			text-decoration-line: none;
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration-line: none;
+			text-decoration: none;
+			color: orange;
+		}
+
+	</style>
+	<section class="bg-light py-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="box">
+						<div class="box-header with-border">
+							<h1 class="box-title">¡404 No encontrado!</h1>
+							<div class="box-tools pull-right">
+								<p>No se encontro la pagina, favor de regresar al menu principal</p>
+							</div>
+						</div>
+						<div>
+							<img width="500px" height="500px" src="public/images/404%20Error-rafiki.svg" alt="404 No encontrado">
+						</div>
+						<div>
+							<a href="index.php">Regresar</a>
+						</div>
+						<a href="https://storyset.com/web">Web illustrations by Storyset</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
